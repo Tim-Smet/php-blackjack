85e6f4c46988217ed4ee11cdb28829f2e79f572c
Tim-Smet <tim2smet@gmail.com>
2020-02-13 16:55:35 +0100

    Getting somewhere

--- blackjack.php
+++ blackjack.php
@@ -4,19 +4,29 @@ class Blackjack {
     //score will keep track of the score at all times.
     public $score = 0;
 
+    public function firstHand(){
+        $card1 = mt_rand(1, 11);
+        $card2 = mt_rand(1, 11);
+        $this->score = $card1 + $card2;
+        return [$card1, $card2];
+    }
     //hit function will add a randomly generated card between 1-11
-    public function hit(){
-        return mt_rand(1, 11);
+    public function hit($currentScore){
+        $randomCard = mt_rand(1, 11);
+        $this->score = $randomCard + $currentScore;
+        $this->score = $_SESSION['score'];
+        return $randomCard;
     }
 
     //End player turn and start dealer's turn
     public function stand(){
-
-
+        echo 'It is the dealers turn. Your score is ' . $_SESSION['score'];
      }
 
      //Give up (Dealer wins)
      public function surrender(){
+        echo 'You gave up, LOSER! Dealer has won!';
+        unset($_SESSION['player']);
 
      }
 }
